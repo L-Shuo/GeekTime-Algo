14bfb4ce9664521dcbd9cee205127a6fe43c4767
liushuo <liushuo0826@sina.com>
2019-07-02 09:05:39 +0800

    Delete b_search node.

--- binary_search_tree.cpp
+++ binary_search_tree.cpp
@@ -42,25 +42,74 @@ TreeNode *search_node(TreeNode *root,int data) {
 		if(data > p->data) {
 			p = p->right;
 		}
-		if(data < p->data) {
+		else if(data < p->data) {
 			p = p->left;
 		}
-		if(data == p->data) {
+		else {
+			printf("%ld\n",p);
 			return p;
 		}
 	}
 	return NULL;
 }
 
-void remove_node(TreeNode *node) {
+void remove_node(Tree *root,TreeNode *node) {
+	TreeNode *p = root;
+	TreeNode *pp = NULL; //parent
+	while( p!= NULL && p->data != node->data) {
+		pp = p;
+		if(node->data > p->data) {
+			p = p->right;
+		}
+		else {
+			p = p->left;
+		}
+	}
+	if( p == NULL) {
+		printf("cannot find target!");
+		return;
+	}
 	
+	if(p->left != NULL && p->right != NULL) {
+		//删除的节点有两个子节点,需要找到右子树中最小节点替换该节点
+		//printf("----1----\n");
+		TreeNode *minP = p->right;
+		TreeNode *minPP = p; //minP's parent
+		while( minP->left != NULL) {
+			minPP = minP;
+			minP = minP->left;
+		}
+		p->data = minP->data;
+		p = minP; //下面代码复用，用来删除minP 
+		pp = minPP;
+	}
+	TreeNode *child;
+	if(p->left != NULL) {
+		child = p->left;
+	}
+	else if(p->right != NULL) {
+		child = p->right;
+	}
+	else
+		child = NULL;
+	if( pp == NULL) {//删除的是root节点 
+		root = child;
+		free(root);
+	}
+	else if(pp->left == p) {
+		pp->left = child;
+	}
+	else {
+		pp->right = child;
+	}
+	free(p);
 }
 
 void _inOrder(TreeNode *root) {
 	if(root == NULL)
 		return;
 	_inOrder(root->left);
-	printf(" %d\n",root->data);
+	printf("%ld %d\n",root,root->data);
 	_inOrder(root->right);
 }
 #if BINARY_SEARCH_TREE
@@ -71,8 +120,8 @@ int main() {
 	root->data = values[0];
 	root->left = NULL;
 	root->right= NULL;
-	
-	for(int i=1;i<9;i++) {
+
+	for(int i=1;i<10;i++) {
 		TreeNode *node = (TreeNode *) malloc(sizeof(TreeNode));
 		node->data = values[i];
 		node->left = NULL;
@@ -80,8 +129,18 @@ int main() {
 		insert_node(root,node);
 	}
 	_inOrder(root);
-	TreeNode *ss = search_node(root,8);
-	if(ss != NULL)
-		printf("find %d\n",ss->data);
+	TreeNode *s1 = search_node(root,22);
+	if(s1 != NULL) {
+		printf("##find %d\n",s1->data);
+		remove_node(root,s1);
+		_inOrder(root);
+	}
+	
+	TreeNode *s2 = search_node(root,2);
+	if(s2 != NULL) {
+		printf("##find %d\n",s2->data);
+		remove_node(root,s2);
+		_inOrder(root);
+	}
 }
 #endif
